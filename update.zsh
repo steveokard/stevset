bcfff5f4b19218919aee05d2a2688f0b46553c31
steveokard <steveokard@gmail.com>
2021-05-29 21:14:20 -0400

    trying out zgenom

--- update.zsh
+++ update.zsh
@@ -4,8 +4,8 @@ rm -r  --interactive=never $HOME/.emacs.d $HOME/.doom.d
 
 echo "Install zinit"
 rm ~/.zshrc ~/.zpreztorc ~/.zlogin ~/.zlogout ~/.zprofile ~/.zshenv
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-#This will install Zinit in ~/.zinit/bin. .zshrc will be updated with three lines of code that wi       │ ll be added to the bottom. The lines will be sourcing zinit.zsh and setting up completion for com       │ mand zinit. After installing and reloading the shell compile Zinit with zinit self-update. 
+git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
+
 ln ~/stevset/zsh/.zshrc ~/.zshrc 
 ln ~/stevset/zsh/aliases.zsh ~/.zsh_aliases
 source ~/.zshrc
